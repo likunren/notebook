dc7530d7f9308590cba05cd1077dd3027c1de4ca
likun Ren <likunr84.ren@gmail.com>
2024-09-01 14:53:57 +0200

    springboot init config

--- src/main/java/com/proberen/www/controller.java
+++ src/main/java/com/proberen/www/controller.java
@@ -1,4 +1,13 @@
 package com.proberen.www;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("login")
 public class controller {
+    @RequestMapping("login")
+    public String login(){
+        return "login";
+    }
 }

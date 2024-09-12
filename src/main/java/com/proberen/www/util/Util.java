8593c158cc67a60abc9bf0a14dfd6ac91767233c
likun Ren <likunr84.ren@gmail.com>
2024-09-12 23:39:36 +0200

    devtools config

--- src/main/java/com/proberen/www/util/Util.java
+++ src/main/java/com/proberen/www/util/Util.java
@@ -1,4 +1,23 @@
 package com.proberen.www.util;
 
+import org.apache.commons.codec.binary.Base64;
+import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.util.UUID;
+
 public class Util {
+    public static String get_UUID(){
+        UUID uuid=UUID.randomUUID();
+        return uuid.toString().toUpperCase().replaceAll("-","");
+    }
+    public static String get_MD5(String str){
+       try{
+           MessageDigest md=MessageDigest.getInstance("MD5");
+           byte[] outPut=md.digest(str.getBytes());
+           String temp= Base64.encodeBase64URLSafeString(outPut);
+           return URLEncoder.encode(temp,"utf-8").toUpperCase();
+       }catch(Exception e){
+           throw new RuntimeException("Verschlüsselung fehlgeschlagen");
+       }
+    }
 }
